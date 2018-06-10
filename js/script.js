c9e0a8b063a9b8cc0457367a43be142611c3f3e1
OlegS <xwx18@mail.ru>
2018-06-10 23:30:08 +0300

    main page full

--- js/script.js
+++ js/script.js
@@ -9,31 +9,31 @@ var mainMenu = document.querySelector('.main-menu');
 var overlay = document.querySelector('.overlay');
 var mainWrapper = document.querySelector('.page-main__wrapper');
 
-openMenu.addEventListener('click', function() {
+openMenu.addEventListener('click', function () {
   mainWrapper.classList.add('page-main__wrapper--open-menu');
   menu.classList.add('mobile-top-menu--open');
-  setTimeout(function() {
+  setTimeout(function () {
     overlay.style.display = 'block';
     overlay.style.left = '300px';
   }, 320);
 });
 
-closeMenu.addEventListener('click', function() {
+closeMenu.addEventListener('click', function () {
   mainWrapper.classList.remove('page-main__wrapper--open-menu');
   menu.classList.remove('mobile-top-menu--open');
   overlay.style.display = 'none';
 });
 
-mainMenu.addEventListener('click', function() {
+mainMenu.addEventListener('click', function () {
   mainWrapper.classList.add('page-main__wrapper--open-menu');
   mainNavigation.classList.add('mobile-main-navigation--open');
-  setTimeout(function() {
+  setTimeout(function () {
     overlay.style.display = 'block';
     overlay.style.left = '300px';
   }, 320);
 });
 
-closeNavigation.addEventListener('click', function() {
+closeNavigation.addEventListener('click', function () {
   mainWrapper.classList.remove('page-main__wrapper--open-menu');
   mainNavigation.classList.remove('mobile-main-navigation--open');
   overlay.style.display = 'none';
@@ -43,6 +43,8 @@ $('.single-item').slick({
   infinite: true,
   speed: 300,
   slidesToShow: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
   dots: true
 });
 
@@ -55,19 +57,31 @@ $('.big-banner').slick({
   dots: true
 });
 
+$('.small-banner').slick({
+  infinite: true,
+  speed: 1500,
+  slidesToShow: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots: true
+});
+
 $('.owl-carousel').owlCarousel({
-  loop:true,
-  margin:55,
-  nav:true,
-  responsive:{
-    0:{
-      items:1
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
+  margin: 55,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1
     },
-    768:{
-      items:2
+    768: {
+      items: 2
     },
-    1200:{
-      items:3
+    1200: {
+      items: 3
     }
   }
 });
